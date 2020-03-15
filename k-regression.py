1a71da61c240eb074c09b360a69b29a0244c3078
Ryo Suzuki <ryo.s12330@gmail.com>
2020-03-15 11:11:13 +0900

    回帰について勉強した

---
+++ k-regression.py
@@ -0,0 +1,49 @@
+# 鬱陶しいFutureWarningを無視
+from warnings import simplefilter
+simplefilter(action='ignore', category=FutureWarning)
+
+from sklearn.datasets import load_breast_cancer
+import pandas as pd
+import numpy as np
+import mglearn
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsRegressor
+
+# mglearn.plots.plot_knn_regression(n_neighbors=3)
+# plt.show()
+
+X, y = mglearn.datasets.make_wave(n_samples=40)
+
+# waveデータセットを訓練セットとテストセットに分ける
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+# 実行
+reg = KNeighborsRegressor(n_neighbors=3)
+reg.fit(X_train, y_train)
+
+# 結果
+print("Test set predictions: \n {}".format(reg.predict(X_train)))
+print("Test set R^2: \n {:.2f}".format(reg.score(X_test, y_test)))
+
+fig, axes = plt.subplots(1, 3, figsize=(15,4))
+line = np.linspace(-3, 3, 1000).reshape(-1, 1)
+for n_neighbors, ax in zip([1, 3, 9], axes):
+    reg = KNeighborsRegressor(n_neighbors=n_neighbors)
+    reg.fit(X_train, y_train)
+    ax.plot(line, reg.predict(line))
+    ax.plot(X_train, y_train, '^', c=mglearn.cm2(0), markersize=8)
+    ax.plot(X_test, y_test, 'v', c=mglearn.cm2(1), markersize=8)
+
+    ax.set_title(
+        "{} neighbar(s)\n train score: {:.2f} test score: {:.2f}".format(
+            n_neighbors, reg.score(X_train, y_train),
+            reg.score(X_test, y_test)
+        )
+    )
+    ax.set_xlabel("Feature")
+    ax.set_ylabel("Target")
+
+axes[0].legend(["Model predictions", "Training data/target", "Test data/target"], loc="best")
+plt.show()

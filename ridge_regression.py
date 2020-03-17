f24159d85dfd4ae57b7a5181939fb635ee27d42b
Ryo Suzuki <ryo.s12330@gmail.com>
2020-03-17 23:28:12 +0900

    リッジかいき

---
+++ ridge_regression.py
@@ -0,0 +1,9 @@
+from main import mglearn, train_test_split
+
+from sklearn.linear_model import Ridge
+X, y = mglearn.datasets.make_wave(n_samples=60)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+ridge = Ridge().fit(X_train, y_train)
+
+print("Training set score: {:.2f}".format(ridge.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(ridge.score(X_test, y_test)))

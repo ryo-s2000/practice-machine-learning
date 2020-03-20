ceb37c84b31b6ff06bdeb071f9342000d72c3c7d
Ryo Suzuki <ryo.s12330@gmail.com>
2020-03-20 11:47:17 +0900

    LinearSVC

---
+++ logisticreRression.py
@@ -0,0 +1,18 @@
+from main import mglearn, train_test_split, plt
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+
+X, y = mglearn.datasets.make_forge()
+
+fig, axes = plt.subplots(1, 2, figsize=(10, 3))
+
+for model, ax in zip([LinearSVC(), LogisticRegression()], axes):
+    clf = model.fit(X, y)
+    mglearn.plots.plot_2d_separator(clf, X, fill=False, eps=0.5, ax=ax, alpha=.7)
+    mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+    ax.set_title("{}".format(clf.__class__.__name__))
+    ax.set_xlabel("Feature 0")
+    ax.set_ylabel("Feature 1")
+axes[0].legend()
+plt.show()
